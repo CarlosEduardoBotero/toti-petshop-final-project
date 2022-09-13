f3efc4d704fe796b070da6c0d1d8f637bc02e0aa
Carlos Botero <boterocev@gmail.com>
2022-09-13 20:05:07 -0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
-
-function App() {
-  return <div className="App">toti-petshop-final-project</div>;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { FaDog } from "react-icons/fa";
+import { FcAssistant } from "react-icons/fc";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <p>
+              home <FaDog className="icon" />
+            </p>
+          }
+        />
+        <Route path="/acessórios" element={<p>acessórios</p>} />
+        <Route path="/adote-me" element={<p>adote</p>} />
+        <Route path="/cerca-de" element={<p>cerca de</p>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
