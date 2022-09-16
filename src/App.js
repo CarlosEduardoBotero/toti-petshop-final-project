718d08c393eabfa996996b6eb5cb759016c1483e
Angelo Arboleda <angeloarboleda33@gmail.com>
2022-09-16 20:18:34 -0300

    componentes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { FaDog } from "react-icons/fa";
 import { FcAssistant } from "react-icons/fc";
+import Accesorios from "./pages/accesorios/Accesorios.";
 const App = () => {
   return (
     <BrowserRouter>
@@ -14,7 +15,7 @@ const App = () => {
             </p>
           }
         />
-        <Route path="/acessórios" element={<p>acessórios</p>} />
+        <Route path="/Accesorios" element={<Accesorios></Accesorios>} />
         <Route path="/adote-me" element={<p>adote</p>} />
         <Route path="/cerca-de" element={<p>cerca de</p>} />
       </Routes>
