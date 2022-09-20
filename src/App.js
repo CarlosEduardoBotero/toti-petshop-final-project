abe3b82c60a551b457b5715ba68f75af4ebb12c9
Madeline Figueroa <madelineafigueroac@gmail.com>
2022-09-20 20:16:19 -0300

    agregar rutas

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { FaDog } from "react-icons/fa";
 import { FcAssistant } from "react-icons/fc";
+import Home from "./pages/Home/Home";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <p>
-              home <FaDog className="icon" />
-            </p>
-          }
-        />
+        <Route path="/" element={<Home></Home>} />
         <Route path="/acessórios" element={<p>acessórios</p>} />
         <Route path="/adote-me" element={<p>adote</p>} />
         <Route path="/cerca-de" element={<p>cerca de</p>} />
